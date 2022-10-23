b82468f50accab316b24229de5a883f4c9ffcba0
Dongubak <91736442+Dongubak@users.noreply.github.com>
2022-10-23 20:42:26 +0900

    todolist
    js공백기, 다시 만들어본 todolist

--- project_TODOLIST/js.js
+++ project_TODOLIST/js.js
@@ -1,28 +1,66 @@
+function getDOM(name) {
+  return document.querySelector(name);
+}
+function createDOM(name) {
+  return document.createElement(name);
+}
+
 function init() {
-    const $input = document.getElementById('input');
-    $input.onkeyup = e => {
-        if (e.key !== 'Enter') {
-            return;
-        }  
-        addlist(e);
-        const $contentListChildren = [...getContentList().children];
-        $contentListChildren.forEach((n) => {
-            n.addEventListener('click', removeList);
-        });
-    }
+  const $btn = getDOM("#btn");
+  $btn.addEventListener("click", onClick);
 }
-function addlist(e) {
-    const $li = document.createElement('li');
-    const $text = document.createTextNode(e.target.value);
-    $li.appendChild($text);
-   getContentList().appendChild($li);
-    e.target.value = '';
+
+function onClick(e) {
+  e.preventDefault();
+  const ul = getDOM(".todo_list");
+
+  ul.appendChild(addItem());
+
+  return 0;
 }
-function removeList(e) {
-    const $contentList = document.getElementById('contentList');
-    $contentList.removeChild(e.target);
+
+function addItem() {
+  const inputDOM = getDOM("#input_text");
+  const randomId = Math.floor(Math.random() * 10000);
+
+  ///add textContent
+  const inputText = inputDOM.value;
+  const $p = createDOM("p");
+  const $li = createDOM("li");
+  $p.textContent = inputText;
+
+  ///add id
+  $li.id = Math.floor(randomId);
+
+  ///add class
+  $li.classList.add("item");
+
+  ///create delete button
+  const $dbtn = createDOM("button");
+  $dbtn.classList.add("buttons");
+  $dbtn.textContent = "삭제";
+  $dbtn.addEventListener("click", () => {
+    onRemove(randomId);
+  });
+  ///delete logic
+  ///1. ul's children의 classname들에서 randomID를 찾는다.
+  ///2. index를 구한 뒤
+
+  ///apply parentsNODE
+  $li.appendChild($p);
+  $li.appendChild($dbtn);
+
+  ///clear for input's content
+  inputDOM.value = "";
+
+  return $li;
 }
-function getContentList() {
-    return document.getElementById('contentList');
+
+function onRemove(randomId) {
+  const lis = getDOM(".todo_list").children;
+  const removeObject = [...lis].find((e) => e.id === String(randomId));
+  const ul = getDOM(".todo_list");
+  ul.removeChild(removeObject);
 }
+
 init();
